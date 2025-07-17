7488d5be95dc6b6f86d33f66d45473be6b8e63ce
LintPillow <kazsaku0804@gmail.com>
2025-07-17 14:00:38 +0900

    H2DBが起動しないのでEntityクラスを修正

--- teamPractice-1/src/main/java/com/example/demo/entity/ArrivalEntity.java
+++ teamPractice-1/src/main/java/com/example/demo/entity/ArrivalEntity.java
@@ -2,13 +2,15 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
+//import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "arrivals")
 public class ArrivalEntity {
 
 	@Id
