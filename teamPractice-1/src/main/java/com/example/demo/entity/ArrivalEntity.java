cbad06c53dbd22e1c340ac925c948262ef2f8660
LintPillow <kazsaku0804@gmail.com>
2025-07-17 13:53:42 +0900

    PunchControllerに処理を追加、Entityクラスを更新

--- teamPractice-1/src/main/java/com/example/demo/entity/ArrivalEntity.java
+++ teamPractice-1/src/main/java/com/example/demo/entity/ArrivalEntity.java
@@ -1,28 +1,43 @@
 package com.example.demo.entity;
 
-import java.security.Timestamp;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-import lombok.Data;
 
 @Entity
-@Table(name = "arrivals")
-@Data
-
-//出勤打刻Entity
 public class ArrivalEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	//	打刻id
 	private Long id;
-	//	ユーザー名
-	private String user_name;
-	//	出勤時間
-	private Timestamp dateTime;
+
+	private String userName;
+	private LocalDateTime timestamp;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
 }
