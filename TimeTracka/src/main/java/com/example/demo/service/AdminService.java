38c64c8be5284723bc55e1242026dd956eeaa42f
ohashi070work <ohahsi070work@gmail.com>
2025-07-18 15:21:04 +0900

    restAPIとDBの接続を実装

--- TimeTracka/src/main/java/com/example/demo/service/AdminService.java
+++ TimeTracka/src/main/java/com/example/demo/service/AdminService.java
@@ -1,5 +1,31 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.ArrivalEntity;
+import com.example.demo.entity.DepartureEntity;
+import com.example.demo.repository.ArrivalRepository;
+import com.example.demo.repository.DepartureRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
 
+    private final ArrivalRepository arrivalRepository;
+    private final DepartureRepository departureRepository;
+    
+    	
+	    public List<ArrivalEntity> getAllArrivals() {
+	    	System.out.println(arrivalRepository.findAll());
+	        return arrivalRepository.findAll(); 
+	    }
+	    
+	    // 退勤打刻 全件取得
+	    public List<DepartureEntity> getAllDepartures() {
+	        return departureRepository.findAll();
+	    }
 }
