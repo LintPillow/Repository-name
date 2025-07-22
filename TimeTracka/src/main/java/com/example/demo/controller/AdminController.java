d3115f2a118a8a511b682d7b6effc66a5d5bc6a2
D006-MKM30B4\ia <aaa@aaa.ocm>
2025-07-22 13:37:03 +0900

    adminログイン処理を追加

--- TimeTracka/src/main/java/com/example/demo/controller/AdminController.java
+++ TimeTracka/src/main/java/com/example/demo/controller/AdminController.java
@@ -3,39 +3,32 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
-		
+
 	@GetMapping("/admin")
 	public String showloginForm() {
 		return "admin/admin";
 	}
-	
-	
-//	@GetMapping("/")
-//	public String authenticate() {
-//		return "adminhome";
-//	}
-	
-	@RequestMapping("/adminhome")
-	public String showDashboard(Model model) {
-		
-		
-		
-		
-		return "admin/adminhome";
-    }
-	
+
+	@PostMapping("/admin")
+	public String authenticate(@RequestParam("username") String username, @RequestParam("password") String password,
+			Model model) {
+
+		if (username.equals("admin") && password.equals("adminpass")) {
+			return "admin/adminhome";
+		} else {
+			model.addAttribute("error", "usernameかpasswordが間違っています");
+			return "/admin/admin";
+		}
+	}
+
 	@RequestMapping("/adminedit")
 	public String editPunch() {
 		return "admin/adminedit";
 	}
-	
-//	@DeleteMapping("/")
-//	public String deletePunch() {
-//		return "adminhome";
-//	}
-
 }
